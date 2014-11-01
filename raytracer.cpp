d98c3e37835911f3c763193b18d00442d30bd6a5
Jerry Lung <jlung@Jerrys-MacBook-Pro-3.local>
2014-10-31 19:18:50 -0700

    final?

--- raytracer.cpp
+++ raytracer.cpp
@@ -369,6 +369,7 @@ int main(int argc, char *argv[]) {
                 translate(1,3) = ty;
                 translate(2,3) = tz;
                 currentTrans->setMat(currentTrans->getMat() * translate);
+                // Transformation* trans = new Transformation();
                 // trans->setMat(translate);
                 // transformations.push_back(trans);
                 numtrans++;
@@ -414,6 +415,7 @@ int main(int argc, char *argv[]) {
                   fprintf(stderr, "Warning: Extra arguments ignored.\n");
                 }
                 currentTrans->setID();
+                transformations.clear();
               }
               else {
                 fprintf(stderr, "Warning: Unsupported feature ignored.\n");
